4a00f2e04199e3af4f8ed6c56f486662e7c26677
notretake <42299902+notretake@users.noreply.github.com>
2025-06-04 02:00:13 -0700

    Update MyToken.sol

--- hardhat/contracts/MyToken.sol
+++ hardhat/contracts/MyToken.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("KAKOOZA", "KAKS") {
+        _mint(msg.sender, 1000 * 10 ** decimals());
+    }
+}
